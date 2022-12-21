007c7e1a3013642670b48508d6eabe4d88b4ba13
yoskoshi <8423koshi@ezweb.ne.jp>
2022-12-21 23:50:12 +0900

    認可画面を表示するURLを定義

--- lib/clients/qiita_client.dart
+++ lib/clients/qiita_client.dart
@@ -1,1 +1,11 @@
-class QiitaClient {}
+import 'package:qiita_kaisetsu/client_information.dart';
+
+class QiitaClient {
+  final clientId = ClientInformation.clientId;
+  final clientSecret = ClientInformation.clientSecret;
+
+  String displayAllowPage(String state) {
+    const scope = "read_qiita%20write_qiita";
+    return "https://qiita.com/api/v2/oauth/authorize?client_id=$clientId&scope=$scope&state=$state";
+  }
+}
