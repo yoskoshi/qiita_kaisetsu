9ee3e1465e9a557b486d80e6d23554ccda24b26f
yoskoshi <8423koshi@ezweb.ne.jp>
2022-12-21 23:08:56 +0900

    トップ画面のUIを作成

---
+++ lib/constraiants/app_color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color backGroundFilterColor = Color(0x33000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color buttonTextColor = Color(0xFF468300);
+}
---
+++ lib/constraiants/app_text.dart
@@ -0,0 +1,6 @@
+class AppText {
+  static const String qiitaFeedApp = "Qiita Feed App";
+  static const String playGround = "-PlayGround-";
+  static const String login = "ログイン";
+  static const String withoutLogin = "ログインせずに利用する";
+}
