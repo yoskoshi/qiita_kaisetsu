9d3f82ff932cda31b077dd59ab0b240a3eaf13aa
yoskoshi <8423koshi@ezweb.ne.jp>
2022-12-23 11:20:46 +0900

    ボトムナビゲーションバーの作成

--- lib/constraiants/app_color.dart
+++ lib/constraiants/app_color.dart
@@ -7,4 +7,6 @@ class AppColor {
   static const Color black = Color(0xFF000000);
   static const Color modalAppColor = Color(0xFFF9F9F9);
   static const Color dividerColor = Color(0x4D000000);
+  static const Color bottomNavigationBarSelectedColor = Color(0xFF74C13A);
+  static const Color bottomNavigationBarUnSelectedColor = Color(0xFF828282);
 }
--- lib/constraiants/app_text.dart
+++ lib/constraiants/app_text.dart
@@ -4,4 +4,8 @@ class AppText {
   static const String login = "ログイン";
   static const String withoutLogin = "ログインせずに利用する";
   static const String qiitaAuth = "Qiita Auth";
+  static const String labelFeed = "フィード";
+  static const String labelTag = "タグ";
+  static const String labelMyPage = "マイページ";
+  static const String labelSetting = "設定";
 }
